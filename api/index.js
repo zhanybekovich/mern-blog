1c5ca889a28813705be53e67ee1bbd3d7aeffe9c
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2021-10-04 20:37:28 +0600

    Lesson 70'

--- api/index.js
+++ api/index.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-
-const app = express();
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
 
 dotenv.config();
+const app = express();
+app.use(express.json());
 
 mongoose
   .connect(process.env.DB_URL, {
-    userNewUrlParser: true,
+    useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(console.log("Connected DB"))
   .catch((err) => console.log(err));
+
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
 
 app.listen("5000", () => {
   console.log("server is running");
